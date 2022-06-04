0e372ecd3de7ff1ce7d932a6437a2ef8feb54782
Tristan <tklieber@myges.fr>
2022-06-04 12:24:28 +0200

    update server.py

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ PORT = 4444
 serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serveur.bind((ADRESSE, PORT))
 serveur.listen(1)
+print("Listening for a new TCP connection ...")
 client, adresseClient = serveur.accept()
 print('Connexion de ', adresseClient)
 
@@ -23,9 +24,11 @@ def handle_client(client):
         if not data:
             print('Erreur de reception. Aucune donnée reçu.\nFermeture du serveur...')
             serveur.close()
+            exit(1)
             break
         if data == b'exit\n':
             serveur.close()
+            exit(1)
             break
         print(data)
     serveur.close()
@@ -33,12 +36,14 @@ def handle_client(client):
 
 _thread.start_new_thread(handle_client, (client,))
 while True:
-    try :
-        data_to_send = input("Entrez ce que vous voulez envoyer ...\n")
-        data_to_send = data_to_send + "\0"
-        data_to_send = data_to_send.encode('utf-8')
+    try:
+        data_to_send = input("Entrez ce que vous voulez envoyer ...\n> ")
         # print(data_to_send)
-        client.send(data_to_send)
+        if data_to_send == "exit":
+            serveur.close()
+            break
+        else:
+            client.send((data_to_send + "\0").encode('utf-8'))
     except KeyboardInterrupt:
         print("\nProgramme interrompu par l'utilisateur\nServer closed...\n")
         serveur.close()
